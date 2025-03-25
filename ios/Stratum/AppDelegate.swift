0d3c2af09972b82f8d72cba23f50a04f0840789b
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-03-26 01:40:46 +0500

    Name changes

--- ios/Stratum/AppDelegate.swift
+++ ios/Stratum/AppDelegate.swift
@@ -7,7 +7,7 @@ import Firebase
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "Link Up"
+    self.moduleName = "Stratum"
     self.dependencyProvider = RCTAppDependencyProvider()
 
     // You can add your custom initial props in the dictionary below.
